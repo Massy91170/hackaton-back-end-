272bae64c43aee9d8471434797f79c78d6a981ca
guilbert <guilbertmartin75@gmail.com>
2024-04-16 16:23:46 +0200

    find avec departure and  arrival ok

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require("./models/connection")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var trajetsRouter = require('./routes/trajets');
 
 var app = express();
 
@@ -16,5 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/trajets', trajetsRouter);
 
 module.exports = app;
--- routes/trajets.js
+++ routes/trajets.js
@@ -1,9 +1,91 @@
 var express = require('express');
 var router = express.Router();
+const Trajet = require("../models/trajets")
+const moment = require('moment');
 
-router.get('/', function(req, res) {
-    
-  res.
-});
+// router.get('/:departure/:arrival/:date', function(req, res) {
+//     const selectedDate = moment.utc(req.params.date).format('YYYY-MM-DD'); // Formater la date spécifiée dans l'URL
+//     const startDate = moment.utc(selectedDate).startOf('day'); // Début de la journée sélectionnée
+//     const endDate = moment.utc(selectedDate).endOf('day'); // Fin de la journée sélectionnée
+//     console.log(startDate)
+//     console.log(endDate)
+
+
+//     Trajet.find({
+//         departure: req.params.departure,
+//         arrival: req.params.arrival,
+//         date: {
+//             $gte: startDate,
+//             $lte: endDate
+//         },
+//         })
+//         .then (data => res.json(data))
+//     });
+
+router.get('/:departure/:arrival/:date', function(req, res) {
+    const selectedDate = moment.utc(req.params.date).format('YYYY-MM-DD'); 
+    const startDate = `${selectedDate}T00:00:00.000+00:00`;
+    const endDate = `${selectedDate}T23:59:59.999+00:00`; 
+    console.log(startDate)
+    console.log(endDate)
+    console.log(selectedDate)
+
+    Trajet.find({
+        departure: req.params.departure,
+        arrival: req.params.arrival,
+        date: {
+            $gte: startDate,
+            $lte: endDate,
+        }
+    })
+    .then(data => res.json(data))
+})
+        // const searchedDate = req.body.date;
+    // const formatedDate = searchedDate.slice(0, 10);
+    // const pattern = /.+?(?=T)/;
+    // const found = searchedDate.match(pattern);
+    // console.log("found: ", found)
+
+
+    // Trajet.find({date: /^.+?(?=T)/})
+    // .then (data => res.json(data))
+
+
+    // console.log("DATE: ", formatedDate)
+    // Trajet.find({
+    //     departure: req.body.departure,
+    //     arrival: req.body.arrival,
+    //     date:{
+    //         $gte: new Date('2024-04-16T00:00:00.000Z'), // Début de la journée
+    //         $lt: new Date('2024-04-17T00:00:00.000Z')   // Fin de la journée
+    //     }
+    // })
+    // .then (data => res.json(data))
+    // Définir la date de début de la journée (00:00:00)
+// const startOfDay = new Date("2024-04-16");
+// endOfDay.setUTCDate(16); // Définir le jour précédent
+// endOfDay.setUTCHours(0, 0, 0, 0); // Définir le début de la journée
+// endOfDay.setMilliseconds(-1)
+
+// // Définir la date de fin de la journée (23:59:59)
+// const endOfDay = new Date("2024-04-16");
+// endOfDay.setUTCDate(16); // Définir le jour précédent
+// endOfDay.setUTCHours(0, 0, 0, 0); // Définir le début de la journée
+// endOfDay.setMilliseconds(-1); // Soustraire 1 milliseconde pour obtenir la fin de la journée précédente
+
+// console.log("endofday :", endOfDay)
+// // Recherche des trajets pour toute la journée du 2024-04-16
+// Trajet.find({
+//     date: {
+//         $gte: startOfDay,
+//         $lte: endOfDay
+//     }
+// })
+// .then (data => res.json(data))
+// });
+
+
+
+
 
 module.exports = router;
